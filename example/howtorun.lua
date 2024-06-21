6e827ad8251bdedb1e1752d7beb80324ff745acd
NilleniumLUA <kasymzhomarttv@gmail.com>
2024-06-21 22:08:01 +0500

    Tutorial to how to run the functions

---
+++ example/howtorun.lua
@@ -0,0 +1,36 @@
+-- 21.06.2024 
+--[[
+##    ## #### ##       ##       ######## ##    ## #### ##     ## ##     ##     ######  ######## ##     ## ########  ####  #######   ######  
+###   ##  ##  ##       ##       ##       ###   ##  ##  ##     ## ###   ###    ##    ##    ##    ##     ## ##     ##  ##  ##     ## ##    ## 
+####  ##  ##  ##       ##       ##       ####  ##  ##  ##     ## #### ####    ##          ##    ##     ## ##     ##  ##  ##     ## ##       
+## ## ##  ##  ##       ##       ######   ## ## ##  ##  ##     ## ## ### ##     ######     ##    ##     ## ##     ##  ##  ##     ##  ######  
+##  ####  ##  ##       ##       ##       ##  ####  ##  ##     ## ##     ##          ##    ##    ##     ## ##     ##  ##  ##     ##       ## 
+##   ###  ##  ##       ##       ##       ##   ###  ##  ##     ## ##     ##    ##    ##    ##    ##     ## ##     ##  ##  ##     ## ##    ## 
+##    ## #### ######## ######## ######## ##    ## ####  #######  ##     ##     ######     ##     #######  ########  ####  #######   ######
+
+All code scripted by NilleniumENT (kasym77777)
+
+LICENSE ACTIVE - COPYING IS DISALLOWED
+AssetID: 18147837388
+]]--
+
+
+
+-- Requires a basic knowledge in running functions.
+
+--[[
+Example:
+local module = require(YOUR_PATH_TO_MODULE_SCRIPT)
+
+module.ANY_FUNCTION_INSIDE_THE_SRC(args,args,args)
+]]--
+
+
+--[[ FULL EXAMPLE:
+
+local module = require(script.Parent.UsefulModules)
+
+
+gh.chat("Random random")
+
+]]--
